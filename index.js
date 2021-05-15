694c0ccbdd0d594d17284e5809d0ee86a8a13d8f
Yesim nur Akar <yesimnurakar@gmail.com>
2021-05-15 14:35:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-// Your code here
-function saturdayFun(act="roller-skate") {
-  console.log(`This Saturday, I want to ${act}!`);
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
 }
-saturdayFun();
-saturdayFun("bathe my dog");
 
-function mondayWork(activity="go to the office") {
-  console.log(`This Monday, I will ${activity}.`);
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
 }
-mondayWork();
-mondayWork("work from home");
+
+let wrapAdjective = function(style="*") {
+  return function(adjective="special") {
+    return `You are ${style}${adjective}${style}!`
+  }
+}
+
+const Calculator = {
+  add: function(a,b) {
+    return a + b;
+  },
+  subtract: function(a,b) {
+    return a - b
+  },
+  multiply: function(a,b) {
+    return a * b
+  },
+  divide: function(a,b) {
+    return a / b;
+  }
+}
+
+let actionApplyer = function(start, ray) {
+  let a = start
+
+  for (let i = 0; i < ray.length; i++ ){
+    a = ray[i](a)
+  }
+
+  return a
+}
