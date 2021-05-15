5a3cfc4e04a737cfa9e15f51f7b7461233d2e0f7
Yesim nur Akar <yesimnurakar@gmail.com>
2021-05-15 14:35:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function saturdayFun(act="roller-skate") {
+  console.log(`This Saturday, I want to ${act}!`);
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(activity="go to the office") {
+  console.log(`This Monday, I will ${activity}.`);
+}
+mondayWork();
+mondayWork("work from home");
